95214bd98683dd8517aaef7ac979b50af9dc8acd
Ubuntu <ubuntu@ip-172-31-23-109.us-west-2.compute.internal>
2016-09-23 22:10:20 +0000

    removed js from main html file

---
+++ public/js/index.js
@@ -0,0 +1,151 @@
+var annotator;
+var annotations = []
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function addAnnotation(left, top, width, height, label) {
+    var html = '<div class="entry">' +
+        '<p class="line"><span class="boldme">Object: </span>' + label + '</p>' +
+        '<p class="line"><span class="boldme">Corner: </span>' + left + 'x' + top + '</p>' +
+        '<p class="line"><span class="boldme">Size: </span>' + width + 'x' + height + '</p>' +
+        '</div>'
+    $('#annotated_box').append(html);
+}
+
+function renderTask(task) {
+    // Can be one of ['text', 'select', 'fixed']
+    var inputMethod = getParameterByName("input");
+    $("#bbox_annotator").empty()
+    $("#annotated_box").empty().append("<h3>Annotations</h3>");
+    // Initialize the bounding-box annotator.
+    annotator = new BBoxAnnotator({
+        url: task.attachment,
+        input_method: "fixed",
+        //input_method: "select",
+        labels: task.objects_to_annotate,
+        onchange: function(entries) {
+            // Input the text area on change. Use "hidden" input tag unless debugging.
+            // <input id="annotation_data" name="annotation_data" type="hidden" />
+            // $("#annotation_data").val(JSON.stringify(entries))
+            //$("#annotation_data").text(JSON.stringify(entries, null, "  "));
+            $("#annotated_box").empty().append("<h3>Annotations</h3>");
+            for (i = 0; i < entries.length; i++) {
+                addAnnotation(entries[i].left, entries[i].top, entries[i].width, entries[i].height, entries[i].label);
+            }
+            annotations = entries;
+            console.log(entries);
+        },
+        radio: $('input:radio:checked')
+    });
+    // Initialize the reset button.
+    $("#reset_button").click(function(e) {
+        annotator.clear_all();
+    })
+    $(document).keypress(function(e) {
+        if (e.which == 114) { // r key press
+            annotator.clear_all();
+        }
+    });
+}
+
+var current_task = {};
+var current_index = 0;
+var all_tasks = [];
+var app = angular.module('myApp', []);
+
+app.factory('socket', ['$rootScope', function($rootScope) {
+    var socket = io.connect();
+
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, callback);
+        },
+        emit: function(eventName, data) {
+            socket.emit(eventName, data);
+        }
+    };
+}]);
+
+app.controller('TaskController', function($scope, socket) {
+    function load_task() {
+        $scope.task_id = current_task._id;
+        $scope.callback_url = current_task.callback_url;
+        $scope.attachment = current_task.attachment;
+        $scope.created_at = current_task.created_at;
+        $scope.instruction = current_task.instruction;
+        $scope.objects_to_annotate = current_task.objects_to_annotate;
+        $scope.urgency = current_task.urgency;
+        $scope.with_labels = current_task.with_labels;
+        renderTask(current_task);
+    }
+
+    $(document).keypress(function(e) {
+        if (e.which == 13 || e.which == 32) { // enter or space
+            e.preventDefault();
+            //current_task = all_tasks[4];
+            //$scope.$apply(load_task);
+            current_task.annotations = annotations;
+            socket.emit("task", current_task);
+        } else if (e.which == 114) { // r key press
+            socket.emit('reset', 'hello');
+        } else if (e.which == 33) {
+            socket.emit('reset', 'hello');
+        }
+    });
+
+    $("#submit-btn").click(function(event) {
+        event.preventDefault();
+        current_task.annotations = annotations;
+        socket.emit("task", current_task);
+    });
+
+    $("#most_important").click(function(event) {
+        socket.emit("most_important");
+    });
+
+    $("#date_created").click(function(event) {
+        socket.emit("date_created");
+    });
+
+    $("#submit_error").click(function(event) {
+        console.log($("#error_message").val());
+        socket.emit("error_msg", {
+            'task': current_task,
+            'error': $("#error_message").val()
+        });
+    });
+
+    $scope.changeTask = function(index) {
+        current_task = all_tasks[index];
+        load_task();
+    };
+    socket.on('connect', function() {
+        $scope.$apply(function() {});
+    });
+    socket.on('message', function(msg) {
+        $scope.$apply(function() {
+            console.log(msg);
+            $scope.message = msg;
+        });
+    });
+    socket.on('task', function(msg) {
+        $scope.$apply(function() {});
+    });
+    socket.on('tasks', function(msg) {
+        $scope.$apply(function() {
+            all_tasks = msg.slice();
+            if (all_tasks.length == 0) {
+                $("#bbox_annotator").empty().append("<h1>You're all done. You can go home now</h1>");
+                $("#annotated_box").empty().append("<h3>Annotations</h3>");
+            }
+            current_task = msg[0];
+            console.log(current_task);
+            load_task();
+            $scope.tasks = msg;
+            console.log(msg);
+        });
+    });
+});
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// server.js
-
 // BASE SETUP
 // =============================================================================
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');
+var app = express(); 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
@@ -15,45 +12,51 @@ var request = require('request');
 var basic = auth.basic({
     realm: 'SUPER SECRET STUFF'
 }, function(username, password, callback) {
-    callback(username == 'APIKEY');
+    callback(username == 'APIKEY'); // Insert your own authentication methods
 });
 
 var authMiddleware = auth.connect(basic);
 
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/imgbox');
 
-var Task= require('./app/models/task');
+var Task = require('./app/models/task');
 
 // Before any of the relevant routes...
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080; // set our port
 
 // Front End
 
-app.get('/', function(req, res){
-  Task.find({status: "pending"}, function(err, tasks) {
-    res.sendfile('index.html');
-  });
+app.get('/', function(req, res) {
+    Task.find({
+        status: "pending"
+    }, function(err, tasks) {
+        res.sendfile('index.html');
+    });
 });
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// Main entry point for image annotations
 router.post('/annotation', authMiddleware, function(req, res) {
-    // create database entry
+    // create database entry of the task
     var task = new Task();
     task.instruction = req.body.instruction;
     task.attachment = req.body.attachment;
@@ -71,8 +74,8 @@ router.post('/annotation', authMiddleware, function(req, res) {
     }
     task.urgency = "day";
     if (req.body.urgency) {
-        if (req.body.urgency == "immediete" || 
-            req.body.urgency == "week" || 
+        if (req.body.urgency == "immediete" ||
+            req.body.urgency == "week" ||
             req.body.urgency == "day") {
             task.urgency = req.body.urgency;
         } else {
@@ -91,7 +94,8 @@ router.post('/annotation', authMiddleware, function(req, res) {
     task.save(function(err) {
         if (err)
             res.send(err);
-        
+
+        // Send the json Response back to requester
         var jsonResponse = {};
         jsonResponse.task_id = task.id;
         jsonResponse.callback_url = task.callback_url;
@@ -105,131 +109,158 @@ router.post('/annotation', authMiddleware, function(req, res) {
         params.objects_to_annotate = task.objects_to_annotate;
         params.with_labels = task.with_labels;
         jsonResponse.params = params;
-        
+
         res.json(jsonResponse);
     });
 });
 
-////////// WEB SOCKET ///////////////
+// WEB SOCKET COMMUNICATION
+// =============================================================================
 
-
-io.on('connection', function(socket){
-  console.log("someone connected");
-  Task.find({status: "pending"}, function(err, tasks) {
-    if (err) {
-        io.emit("message", err);
-    } else {
-        io.emit("task", tasks[0]);
-        io.emit("tasks", tasks);
-    }
-  });
-  socket.on('task', function(data) {
-    // put into data base, completed_at 
-    Task.findOne({ _id: data._id}, function(err, task) {
-      if (err) {
-        io.emit("message", err);
-      } else {
-          io.emit("message", "Task found, starting to save to database...");
-          task.completed_at = new Date();
-          task.response = {annotations: data.annotations};
-          task.status = "completed";
-          task.save(function (err) {
-            if (err) {
-                io.emit("message", err);
-            } else {
-                io.emit("message", "Saved to database successfully, now sending request...");
-                var jsonResponse = {};
-                var mytask  = JSON.parse(JSON.stringify(task));
-                var response = {};
-                var mytaskid = task._id
-                response = task.response;
-                delete mytask.api_key;
-                mytask.task_id = task._id;
-                delete mytask._id;
-                jsonResponse.task = mytask;
-                jsonResponse.response = response;
-                jsonResponse.task_id = mytaskid;
-                var options = {
-                    uri: task.callback_url,
-                    method: 'POST',
-                    json: jsonResponse
-                };
-                request(options, function (err, response, body) {
-                    if (err) {
-                        io.emit("message",  "Error sending request");
-                    } else {
-                          Task.find({status: "pending"}, function(err, tasks) {
-                            if (err) {
-                                io.emit("message", err);
-                            } else {
-                                io.emit("tasks", tasks);
-                            }
-                          });
-                        io.emit("message", "Successfully sent request.");
-                    }
-                });
-            }
-          });
-      }
-    });
-  });
-  socket.on('error_msg', function(data) {
-    console.log(data);
-    var options = {
-        uri: data.task.callback_url,
-        method: 'POST',
-        json: {'error': data.error}
-    };
-    request(options, function (err, response, body) {
-        if (err) {
-            io.emit("message",  "Error sending request");
-        } else {
-            io.emit("message", "Error successfully sent.");
-        }
-    });
-  });
-
-  function urgency(doc1, doc2) {
-    if (doc1.urgency == doc2.urgency) {
-        
-    }
-  };
-  socket.on('most_important', function() {
-      Task.find({urgency: "immediete"}, function(err, tasks) {
+io.on('connection', function(socket) {
+    console.log("someone connected");
+    Task.find({
+        status: "pending"
+    }, function(err, tasks) {
         if (err) {
             io.emit("message", err);
         } else {
-            Task.find({urgency: "day"}, function(err, tasks2) {
-                if (err) {
-                    io.emit("message", err);
-                } else {
-                    Task.find({urgency: "week"}, function(err, tasks3) {
-                        io.emit("tasks", tasks.concat(tasks2.concat(tasks3)))
-                    });
-                }
-            });
+            io.emit("task", tasks[0]);
+            io.emit("tasks", tasks);
         }
-      });
-  });
-  socket.on('date_created', function() {
-    Task.find().sort('created_at').exec(function(err, tasks) {
-        io.emit("tasks", tasks);
     });
-  });
-  socket.on('reset', function() {
-    Task.update({status:'completed'}, {status: 'pending'}, {multi: true}, 
-        function(err, num) {
-            console.log("updated "+num);
-        }
-    );
-  });
+
+    // When completed a task, will try to save to Database
+    // and send POST request back to callback_url
+    socket.on('task', function(data) {
+
+        // Modify task in mongodb
+        Task.findOne({
+            _id: data._id
+        }, function(err, task) {
+            if (err) {
+                io.emit("message", err);
+            } else {
+                io.emit("message", "Task found, starting to save to database...");
+                task.completed_at = new Date();
+                task.response = {
+                    annotations: data.annotations
+                };
+                task.status = "completed";
+                task.save(function(err) {
+                    if (err) {
+                        io.emit("message", err);
+                    } else {
+
+                        // Send request to callback url
+                        io.emit("message", "Saved to database successfully, now sending request...");
+                        var jsonResponse = {};
+                        var mytask = JSON.parse(JSON.stringify(task));
+                        var response = {};
+                        var mytaskid = task._id
+                        response = task.response;
+                        delete mytask.api_key;
+                        mytask.task_id = task._id;
+                        delete mytask._id;
+                        jsonResponse.task = mytask;
+                        jsonResponse.response = response;
+                        jsonResponse.task_id = mytaskid;
+                        var options = {
+                            uri: task.callback_url,
+                            method: 'POST',
+                            json: jsonResponse
+                        };
+                        request(options, function(err, response, body) {
+                            if (err) {
+                                io.emit("message", "Error sending request");
+                            } else {
+                                Task.find({
+                                    status: "pending"
+                                }, function(err, tasks) {
+                                    if (err) {
+                                        io.emit("message", err);
+                                    } else {
+                                        io.emit("tasks", tasks);
+                                    }
+                                });
+                                io.emit("message", "Successfully sent request.");
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    // When the front end sends an error about the task, send it back to callback_url
+    socket.on('error_msg', function(data) {
+        console.log(data);
+        var options = {
+            uri: data.task.callback_url,
+            method: 'POST',
+            json: {
+                'error': data.error
+            }
+        };
+        request(options, function(err, response, body) {
+            if (err) {
+                io.emit("message", "Error sending request");
+            } else {
+                io.emit("message", "Error successfully sent.");
+            }
+        });
+    });
+
+    // Sort by Urgency
+    socket.on('most_important', function() {
+        Task.find({
+            urgency: "immediete"
+        }, function(err, tasks) {
+            if (err) {
+                io.emit("message", err);
+            } else {
+                Task.find({
+                    urgency: "day"
+                }, function(err, tasks2) {
+                    if (err) {
+                        io.emit("message", err);
+                    } else {
+                        Task.find({
+                            urgency: "week"
+                        }, function(err, tasks3) {
+                            io.emit("tasks", tasks.concat(tasks2.concat(tasks3)))
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    // Sort by Date Created
+    socket.on('date_created', function() {
+        Task.find().sort('created_at').exec(function(err, tasks) {
+            io.emit("tasks", tasks);
+        });
+    });
+
+    // Dubug purposes, should not be used
+    socket.on('reset', function() {
+        Task.update({
+                status: 'completed'
+            }, {
+                status: 'pending'
+            }, {
+                multi: true
+            },
+            function(err, num) {
+                console.log("updated " + num);
+            }
+        );
+    });
 });
 
 
-// more routes for our API will happen here
-
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api/task', router);
 
 app.use(express.static('public'));
